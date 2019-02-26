cdc5250ebe3687df2185f2647aa540db7b554b4d
brad-johnson2 <brad.johnson597@gmail.com>
2019-02-26 08:34:07 -0600

    init commit

---
+++ CustomList-master/CustomList/CustomList/Class1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomList
+{
+    public class CustomList<T>
+    {
+        private int count = 0;
+        public int index;
+        public int capacity = 4;
+        public T[] array;
+
+        public CustomList()
+        {
+            array = new T[capacity];
+        }
+
+
+        public int Count
+        {
+            get
+            {
+                return count;
+            }
+        }
+
+
+        //count property online property
+
+
+        //indexer
+
+        public T this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+
+            set => array[index] = value;
+        }
+        //add (capacity)
+
+        public T[] IncreaseCapacity()
+        {
+            //int oldSize = array.Length;
+            //int newSize = capacity * 2;
+            T[] newArray = new T[capacity * 2];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            //if (newArray.Length > array.Length)
+            //{
+
+            //    newArray = array;
+            //    return array;
+            //}
+
+            //}
+            array = newArray;
+            return array;
+
+
+
+            //double array, new array and erase old array, 
+        }
+
+        public void Add(T input)
+        {
+
+
+
+            if (count >= capacity)
+            {
+                IncreaseCapacity();
+                array[count] = input;
+                count++;
+            }
+            else
+            {
+                array[count] = input;
+                count++;
+            }
+
+        }
+
+        public T[] Remove(T input)
+        {
+            T[] newArray = new T[capacity];
+            bool toggle = false;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Equals(input) && toggle == false)
+                {
+                    toggle = true;
+                }
+                else
+                {
+                    //newArray.Add(array[i]);
+                    count++;
+                }
+                break;
+            }
+            //count--;
+            array = newArray;
+            return array;
+
+
+        }
+
+
+        //ToString
+
+        //overload add for two instances
+
+        //overload remove
+
+        //zipper 
+
+        //iterator
+
+
+
+    }
+}
---
+++ CustomList-master/CustomList/CustomListTests/UnitTest1.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomList;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void Add_CheckCount_Increases()
+        {
+            //        -	Check if count increases
+
+            //Arrange
+
+            CustomList<int> list = new CustomList<int>();
+
+            int itemOne = 5;
+
+            int expectedResult = 1;
+
+
+            //Act
+            list.Add(itemOne);
+            int actualResult = list.Count;
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void Add_AddValue_CorrectIndex()
+        {
+
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            // int correctIndex;
+            int itemOne = 5;
+            int expectedResult = 5;
+
+            //Act
+            list.Add(itemOne);
+            int actualResult = list[0];
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void Add_StringValue_CorrectString()
+        {
+            //-	Check if string value is correctly added
+
+            //Arrange
+            CustomList<string> list = new CustomList<string>();
+
+            string itemOne = "5";
+
+
+            string expectedResult;
+            string actualResult;
+            expectedResult = "5";
+
+            //Act
+            list.Add(itemOne);
+            actualResult = list[0];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+
+        [TestMethod]
+        public void Add_AddItems_LastIndex()
+        {
+            //-	Check if each added item appears at the last index of array
+
+            CustomList<int> list = new CustomList<int>();
+
+            int itemOne = 5;
+            int itemTwo = 3;
+            int expectedResult;
+            int actualResult;
+            expectedResult = 3;
+
+            //Arrange
+
+
+
+            //Act
+
+            list.Add(itemOne);
+            list.Add(itemTwo);
+            actualResult = list[list.Count - 1];
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void Add_AddValue_DoesNotDisturbOtherValues()
+        {
+            //-	
+
+            //Arrange
+
+            CustomList<int> list = new CustomList<int>();
+
+            int itemOne = 5;
+            int itemTwo = 2;
+            int actualResult;
+            int expectedResult;
+            expectedResult = 5;
+
+            //Act
+            list.Add(itemOne);
+            list.Add(itemTwo);
+
+            actualResult = list[0];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+        [TestMethod]
+        public void Check_Capacity_DoublesCapacity()
+        {
+            //-	
+
+            //Arrange
+
+            CustomList<int> list = new CustomList<int>();
+
+            int itemOne = 5;
+            int itemTwo = 2;
+            int itemThree = 14;
+            int itemFour = 6;
+            int itemFive = 127;
+            int actualResult;
+            int expectedResult;
+            expectedResult = 127;
+
+
+            //Act
+            list.Add(itemOne);
+            list.Add(itemTwo);
+            list.Add(itemThree);
+            list.Add(itemFour);
+            list.Add(itemFive);
+
+            actualResult = list[4];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+
+        [TestMethod]
+        public void Sub_RemoveItem_DecreaseCount()
+        {
+            //-	Check if count decreases
+
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+
+            int itemOne = 5;
+            int itemTwo = 2;
+            int actualResult;
+            int expectedResult = 1;
+
+
+            //Act
+            list.Add(itemOne);
+            list.Add(itemTwo);
+            list.Remove(itemTwo);
+
+
+
+            actualResult = list.Count;
+
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+
+        [TestMethod]
+        public void Sub_RemoveItem_SpecificIndex()
+        {
+            //-	Check for value removed at specific index
+
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+
+            int itemOne = 5;
+            int itemTwo = 2;
+            int expectedResult;
+            int actualResult;
+
+            //Act
+            list.Add(itemOne);
+            list.Add(itemTwo);
+            list.RemoveAt(0);
+            actualResult = list[0];
+            expectedResult = 2;
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+
+        [TestMethod]
+        public void Sub_LastIndex_ReturnsException()
+        {
+            //-	Check if last index of array returns exception
+
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            int itemOne = 3;
+            int itemTwo = 5;
+
+
+            //Act
+            list.Add(itemOne);
+            list.Add(itemTwo);
+            list.Remove(itemTwo);
+            Console.WriteLine(list[1]);
+
+            //Assert
+
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => Remove.Sub_LastIndex_ReturnsException());
+
+
+        }
+
+
+        [TestMethod]
+        public void Sub_RemoveItem_DoesNotDisturb()
+        {
+            //-	Check if removing item doesn't disturb other items
+
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            int itemOne = 3;
+            int itemTwo = 5;
+            int expectedResult = 3;
+            int actualResult;
+
+
+            //Act
+            list.Add(itemOne);
+            list.Add(itemTwo);
+            list.Remove(itemTwo);
+            actualResult = list[0];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+
+        [TestMethod]
+        public void Sub_RemoveItem_RemovesIntended()
+        {
+            //-	Check if removing item doesn't disturb other items
+            // remove method always removes the first instance it encounters.
+
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            int itemOne = 3;
+            int itemTwo = 5;
+            int itemThree = 6;
+            int itemFour = 5;
+
+            int expectedResult = 6;
+            int actualResult;
+
+
+            //Act
+            list.Add(itemOne);
+            list.Add(itemTwo);
+            list.Add(itemThree);
+            list.Add(itemFour);
+            list.Remove(5);
+            actualResult = list[1];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+
+
+        //        -	Check that every index of new array is string
+
+        [TestMethod]
+        public void ToString_StringItem_IsString()
+        {
+           
+
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            int itemOne = 3;
+            int itemTwo = 5;
+           
+
+            int expectedResult = "5";
+            int actualResult;
+
+
+            //Act
+            list.Add(itemOne);
+            list.Add(itemTwo);
+            
+            list.ToString(5);
+            actualResult = list[1];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+
+
+        //-	Check that strings converted to strings are still strings
+
+        [TestMethod]
+        public void ToString_StartingString_IsString()
+        {
+
+
+            //Arrange
+            CustomList<int> list = new CustomList<int>();
+            string itemOne = "3";
+            string itemTwo = "5";
+
+
+            int expectedResult = "5";
+            int actualResult;
+
+
+            //Act
+            list.Add(itemOne);
+            list.Add(itemTwo);
+
+            list.ToString("5");
+            actualResult = list[1];
+
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+
+        }
+
+        //-	Check that items remained at same index
+        //-	Check to see that int as strings are still strings
+        //-	Check to see that count is the same
+
+
+
+
+
+    }
+}
